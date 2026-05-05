6a8f69cf6bc2c265772ca13223126d2928ba6072
Shreya <shreya.hegde2007@gmail.com>
2026-05-05 15:25:36 +0530

    10_matplotlib

---
+++ 10_matplotlib/04_basics.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+ypoints=np.array([24,5,8,45,3])
+
+plt.plot(ypoints)
+plt.show()
---
+++ 10_matplotlib/06_basics.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+xpoints=np.array([4,7,23,55,9])
+ypoints=np.array([24,5,8,45,3])
+
+plt.plot(xpoints,ypoints,"*:b")
+plt.show()
